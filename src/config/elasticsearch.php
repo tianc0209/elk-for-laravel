6000d39340b6587d37cd14c061188f6193154a32
tianc <tianc@51cashbox.com>
2019-10-15 20:32:20 +0800

    fix config bug

--- src/config/elasticsearch.php
+++ src/config/elasticsearch.php
@@ -7,5 +7,5 @@ return [
 
     'log_index' => env('ELASTIC_LOG_INDEX','bf_log'),
     'log_type' => env('ELASTIC_LOG_TYPE','log'),
-    'use_job' => env('ELASTIC_USE_JOB',false),
+    'use_job' => env('ELASTIC_USE_QUEUE',false),
 ];
